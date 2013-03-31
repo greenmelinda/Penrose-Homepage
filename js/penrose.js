db6e2f791aa6a2e0af56dd68235ee4120c51bcbe
bcourter <bcourter@mac.com>
2013-03-31 12:41:27 -0400

    Working rotating penrose

--- js/penrose.js
+++ js/penrose.js
@@ -38,7 +38,15 @@ function init() {
 	];
 	
 	
-	bodyGeometry = new THREE.ObjGeometry( "resources/obj/penrose-body.obj", 1 );
+	var loader = new THREE.OBJLoader();
+	loader.addEventListener( 'load', function ( event ) {
+		bodyGeometry = event.content;
+		var scale = 0.025;
+		bodyGeometry.scale.set(scale, scale, scale);
+		bodyGeometry.position.y = -2.2;
+	});
+	
+    loader.load( "resources/obj/penrose-body.obj" );
 	
 	settings = new Settings();	
 }
@@ -62,9 +70,15 @@ function render() {
 	var time = new Date().getTime() / 1000;
 	var scene = new THREE.Scene();
 	
+	
 //	var shape = THREE.SceneUtils.createMultiMaterialObject( bodyGeometry, materials );
 //	shape.overdraw = true;
-	scene.add(bodyGeometry);
+	scene.add(bodyGeometry.clone());
+	
+	// sphere
+//	var sphere = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshLambertMaterial({color: 0x0000ff}));
+	//sphere.overdraw = true;
+	//scene.add(sphere);
 	
 	// add subtle ambient lighting
 	var ambientLight = new THREE.AmbientLight(0x555555);
